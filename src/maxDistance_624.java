1398228d0f8c7ba92ce93298f667f5dfb57aae25
Yan Zixin <153819742+ZixinYan@users.noreply.github.com>
2025-02-19 13:46:14 +0800

    Create maxDistance_624.java

---
+++ src/maxDistance_624.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class maxDistance_624 {
+    public int maxDistance(List<List<Integer>> arrays) {
+        int min = arrays.get(0).get(0);
+        int max = arrays.get(0).get(arrays.get(0).size()-1);
+        int res = 0;
+        for (int i = 1; i < arrays.size(); i++) {
+            int n = arrays.get(i).size();
+            res = Math.max(res, Math.max(Math.abs(arrays.get(i).get(n - 1) - min),
+                    Math.abs(max- arrays.get(i).get(0))));
+            min = Math.min(min, arrays.get(i).get(0));
+            max = Math.max(max, arrays.get(i).get(n - 1));
+        }
+        return res;
+    }
+}
